a3c4b905a0fe16b6cd893142dbedb8cc6b1c8544
awesomergs <george.rohan21@gmail.com>
2026-01-10 13:57:12 -0800

    learning onDelete

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,47 +8,74 @@
 import Observation
 import SwiftUI
 
-@Observable //A 'macro' - Swifts way of rewriting our code for additional functionality. Can right click and go to 'expand macro'
-class User{
-    var firstName = "Bilbo"
-    var lastName = "Baggins"
-}
-
-struct SecondView: View {
-    @Environment(\.dismiss) var dismiss
-    let name: String
-    
-    var body: some View{
-        // Text("Hello, \(name)!")
-        Button("Dismiss"){
-            dismiss()
-        }
-    }
-}
+// @Observable //A 'macro' - Swifts way of rewriting our code for additional functionality. Can right click and go to 'expand macro'
+//class User{
+//    var firstName = "Bilbo"
+//    var lastName = "Baggins"
+//}
+//
+//struct SecondView: View {
+//    @Environment(\.dismiss) var dismiss
+//    let name: String
+//    
+//    var body: some View{
+//        // Text("Hello, \(name)!")
+//        Button("Dismiss"){
+//            dismiss()
+//        }
+//    }
+//}
 
 struct ContentView: View {
-    @State private var user = User()
     @State private var showingSheet = false
+    @State private var numbers: [Int] = []
+    @State private var currentNumber = 1
     
     var body: some View {
-        VStack{
-            TextField("First Name:", text: $user.firstName)
-                .padding(20)
-            TextField("Last Name:", text: $user.lastName)
-                .padding(20)
-            
-            Spacer()
-            
-            Button("Show Sheet"){
-                showingSheet.toggle()
+        NavigationStack{
+            VStack{
+                List{
+                    ForEach(numbers, id: \.self){
+                        Text("Row \($0)")
+                    }
+                    .onDelete(perform: removeRows(_:))
+                }
+                
+                
+                Button("Add Number"){
+                    numbers.append(currentNumber)
+                    currentNumber += 1
+                }
+                
             }
-            .sheet(isPresented: $showingSheet){
-                SecondView(name: user.firstName + " " + user.lastName)
+            .toolbar{
+                EditButton()
             }
         }
     }
+    
+    func removeRows(_ offsets: IndexSet){
+        numbers.remove(atOffsets: offsets)
+    }
+    
 }
 
 #Preview {
     ContentView()
 }
+
+
+
+//            TextField("First Name:", text: $user.firstName)
+//                .padding(20)
+//            TextField("Last Name:", text: $user.lastName)
+//                .padding(20)
+//
+//            Spacer()
+//
+//            Button("Show Sheet"){
+//                showingSheet.toggle()
+//            }
+//            .sheet(isPresented: $showingSheet){
+//                SecondView(name: user.firstName + " " + user.lastName)
+//            }
